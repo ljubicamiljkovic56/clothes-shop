a0c5780f5707710d844f5b72de9fe7ac120a9303
ljubicamiljkovic56 <ljubica.m19@gmail.com>
2024-07-30 16:30:48 +0200

    Added shop.json file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $('.back-to-top').click(function(){
     $('html, body').animate({scrollTop: 0}, 1000);
 });
+$.ajax({
+    url: "shop.json",
+    method: "get",
+    dataType: "json"
+})
+.done(function(res){
+    console.log(res);
+})
