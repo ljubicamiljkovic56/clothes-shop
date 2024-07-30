6f719dde0269f825d3c924c117584c4d31e2bc74
ljubicamiljkovic56 <ljubica.m19@gmail.com>
2024-07-30 18:33:03 +0200

    Tried to make a template to fetch data from json file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var template = $('#template').html();
+console.log(template);
 $('.back-to-top').click(function(){
     $('html, body').animate({scrollTop: 0}, 1000);
 });
@@ -8,4 +10,12 @@ $.ajax({
 })
 .done(function(res){
     console.log(res);
+    var text = "";
+    res.forEach(function (e){
+        text = template.replace("{{imgSrc}}", e.imgSrc)
+        .replace("{{productTitle}}", e.productTitle)
+        .replace("{{model}}", e.model)
+        .replace("{{price}}", e.price);
+    $("#mainRow").append(text);
+    })
 })
