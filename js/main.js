cf380e5db11a8cdf8d2fd18dbbcdec60f95308dc
ljubicamiljkovic56 <ljubica.m19@gmail.com>
2024-08-13 14:02:46 +0200

    Changed main.js, more clean code. Finished with project

--- js/main.js
+++ js/main.js
@@ -4,10 +4,29 @@ console.log(template);
 var collections = $("[data-collection]");
 var mainRow = $('#mainRow');
 console.log(collections.length);
-collections.on('click', displayCollections);
-display();
 
-function display(){
+window.onload = function () {
+    collections.parent().removeClass('active')
+    $.ajax({
+        url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json",
+        method: "get",
+        dataType: "json"
+    })
+    .done(function(res) {
+        displayCollections(res);
+        collections.on('click', function(){
+            displayCollections.call(this,res);
+        })
+    })
+}
+
+$('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop: 0}, 1000);
+});
+
+//window.onload = displayCollections;
+//display();
+/*function display(){
     mainRow.html("");
     $('.back-to-top').click(function(){
         $('html, body').animate({scrollTop: 0}, 1000);
@@ -28,49 +47,40 @@ function display(){
         $("#mainRow").append(text);
         })
     })
-}
+} */
 
-function displayCollections(e){
+function displayCollections(res){
     mainRow.html("");
-    e.preventDefault();
+    event.preventDefault();
     var col = $(this).data('collection');
     console.log(col);
-    $('.back-to-top').click(function(){
-        $('html, body').animate({scrollTop: 0}, 1000);
-    });
-    $.ajax({
-        url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json",
-        method: "get",
-        dataType: "json"
+
+    if(col === 'male' || col === 'female'){
+        var colFilter = res.filter(function (el){
+             return el.colection === col;
+        })
+        console.log(res);
+        displayProduct(colFilter);
+    }else if(col === 'newCol' || col === 'popular' || col === 'action'){
+        collections.parent().removeClass('active');
+        $(this).parent().addClass('active');
+        var colFilter = res.filter(function (el) {
+         return el[col];
     })
-    .done(function(res){
-        if(col === 'male' || col === 'female'){
-            var colFilter = res.filter(function (el){
-                return el.colection === col;
-            })
-            console.log(res);
-            var text = "";
-            colFilter.forEach(function (e){
-                text = template.replace("{{imgSrc}}", e.imgSrc)
+        displayProduct(colFilter);
+    } else {
+        displayProduct(res);
+    }
+}
+
+function displayProduct(filter){
+    var text = "";
+        filter.forEach(function (e){
+        console.log(e);
+        text = template.replace("{{imgSrc}}", e.imgSrc)
                 .replace("{{productTitle}}", e.productTitle)
                 .replace("{{model}}", e.model)
                 .replace("{{price}}", e.price);
-            $("#mainRow").append(text);
-            })
-        }else if(col === 'newCol' || col === 'popular' || col === 'action'){
-            var colFilter = res.filter(function (el) {
-                return el[col];
-            })
-            var text = "";
-            colFilter.forEach(function (e){
-                console.log(e);
-                text = template.replace("{{imgSrc}}", e.imgSrc)
-                    .replace("{{productTitle}}", e.productTitle)
-                    .replace("{{model}}", e.model)
-                    .replace("{{price}}", e.price);
-                mainRow.append(text);
-        })
-        }
-
+        mainRow.append(text);
     })
 }
